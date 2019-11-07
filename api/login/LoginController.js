fea67063080919115a205b2883df84ac512b1032
Douglas Rafael Viehbeck <douglas.viehbeck@universo.univates.br>
2019-11-06 22:49:41 -0200

    Add user authentication and token verification to login microservice

--- api/login/LoginController.js
+++ api/login/LoginController.js
@@ -1,28 +1,41 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const DBHelper = require('./DBHelper');
 
 module.exports = {
-    authenticate: (req, res) => {
-        let { user, password } = req.body;
-
-        if(user === 'douglas' && password === '123'){
-            const id = 1; // ID BANCO
-
-            console.log(process.env);
-
-            var token = jwt.sign({ id }, process.env.SECRET, {
-                expiresIn: '7d',
-            });
-
-            return res.status(200).send({ auth: true, token: token });
-        }
+    authenticate: async (req, res) => {
+        const db = await DBHelper.getConnection();
         
-        res.status(200).send({ auth: false });
+        let { email, password } = req.body;
+
+        let user = (await db.query('SELECT * FROM users WHERE email = $1', [email])).rows[0];
+
+        if (!user) {
+            return res.status(200).send({ auth: false });
+        }
+
+        const id = user.id;
+
+        bcrypt.compare(password, user.password, async (err, valid) => {
+            if (valid) {
+                var token = jwt.sign({ id }, process.env.SECRET, {
+                    expiresIn: '7d',
+                });
+                
+                res.status(200).send({ auth: true, token: token });
+            } else {
+                res.status(200).send({ auth: false });
+            }
+        });
     },
     verify: (req, res) => {
-        jwt.verify(req.body.token, process.env.SECRET, function(err, decoded) {
+        jwt.verify(req.body.token, process.env.SECRET, async (err, decoded) => {
             if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+            const db = await DBHelper.getConnection();
             
-            res.status(200).send({ auth: true, user: {a: 1} });
+            let user = (await db.query('SELECT name, email, cpf FROM users WHERE id = $1', [decoded.id])).rows[0];
+
+            res.status(200).send({ auth: true, user });
         });
     },
 };
